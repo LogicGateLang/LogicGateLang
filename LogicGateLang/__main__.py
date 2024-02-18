dbf37f3537fd61cbf5040d08e17537de7ee1d697
Michal <kowal_@outlook.com>
2024-02-18 17:12:04 +0100

    made the structure for the project

---
+++ LogicGateLang/__main__.py
@@ -0,0 +1,6 @@
+# Importing the lgl modules
+import lglCore as core
+from lglCore import lglTypes as types
+import lglCLI as cli
+
+
---
+++ LogicGateLang/lglCore/lglTypes.py
@@ -0,0 +1,16 @@
+gates = {
+    "and": "AND",
+    "or": "OR",
+    "not": "NOT",
+    "nand": "NAND",
+    "nor": "NOR",
+    "xor": "XOR",
+    "xnor": "XNOR",
+    "buf": "BUF"
+}
+functions = {
+    "set": "SET",
+    "reset": "RESET",
+    "toggle": "TOGGLE",
+    "print": "PRINT",
+}
